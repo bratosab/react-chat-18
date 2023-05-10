ab9c278646e096de6b9f7f835db94c3b05a76727
Abdallah <bratosab@gmail.com>
2023-05-10 16:28:40 +0200

    feat(Pending): Filtered out own pending message

--- src/components/Pending.js
+++ src/components/Pending.js
@@ -1,14 +1,19 @@
-import { collection, orderBy, query } from "firebase/firestore";
-import { store } from "../App";
+import { collection, orderBy, query, where } from "firebase/firestore";
+import { auth, store } from "../App";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
-export function Pending({pendingCollection}) {
-    const q = query(pendingCollection, orderBy("timestamp"));
-    const [pendings] = useCollectionData(q);
+export function Pending({ pendingCollection }) {
+  const q = query(
+    pendingCollection,
+    where("uid", "!=", auth.currentUser.uid),
+    orderBy("uid", "timestamp")
+  );
+  const [pendings] = useCollectionData(q);
 
-    return (
-        <div>
-            { pendings && pendings.map(pending => <span>{pending.name} is writing...</span>) } 
-        </div>
-    )
+  return (
+    <div>
+      {pendings &&
+        pendings.map((pending) => <span>{pending.name} is writing...</span>)}
+    </div>
+  );
 }
